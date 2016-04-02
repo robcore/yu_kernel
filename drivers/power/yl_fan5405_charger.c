472163dd8247cf30e6f36d02e22745f9084338b2
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2016-04-02 23:34:36 +0530

    Update yl_fan5405_charger.c

--- drivers/power/yl_fan5405_charger.c
+++ drivers/power/yl_fan5405_charger.c
@@ -1342,10 +1342,8 @@ static void fan5405_external_power_changed(struct power_supply *psy)
 	if (rc < 0)
 		dev_err(chip->dev,
 			"could not read USB current_max property, rc=%d\n", rc);
-<<<<<<< HEAD
 	else
 		chip->set_ivbus_max = prop.intval / 1000;
-=======
 	else {
 #ifdef CONFIG_THUNDERCHARGE_CONTROL
         if(!((prop.intval / 1000) == 0))
@@ -1371,7 +1369,6 @@ static void fan5405_external_power_changed(struct power_supply *psy)
 #endif
 
 		}
->>>>>>> 827a39a... thundercharge control v2.0
 
 
 	rc = fan5405_set_ivbus_max(chip, chip->set_ivbus_max); //VBUS CURRENT
@@ -1441,12 +1438,9 @@ static int fan5405_parse_dt(struct fan5405_chip *chip)
 	if (rc < 0)
 		return -EINVAL;
 
-<<<<<<< HEAD
-=======
 #ifdef CONFIG_THUNDERCHARGE_CONTROL
 	chip->chg_curr_max = custom_ac_current;
 #else
->>>>>>> 827a39a... thundercharge control v2.0
 	rc = of_property_read_u32(node, "yl,max-charge-current-mA", &chip->chg_curr_max);
 	if (rc < 0)
 		return -EINVAL;
